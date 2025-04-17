d204e78924031f1c716061eddd752276cdc9f9ec
vhemeret <valhmrt@gmail.com>
2025-04-17 21:13:02 +0200

    Grace done.

--- Python/Grace.py
+++ Python/Grace.py
@@ -1,8 +1,10 @@
-
+# The alone comment
 def quine():
-    try:
-        open('Grace_kid.py', 'w')
-    except Exception as e:
-        print('Error: ', {e})
+	str = "# The alone comment\ndef quine():\n\tstr = {!r}\n\ttry:\n\t\twith open('Grace_kid.py', 'w') as lilGrace:\n\t\t\tlilGrace.write(str.format(str))\n\texcept Exception as e:\n\t\tprint('Error: ', {{e}})\n\nquine()"
+	try:
+		with open('Grace_kid.py', 'w') as lilGrace:
+			lilGrace.write(str.format(str))
+	except Exception as e:
+		print('Error: ', {e})
 
 quine()
