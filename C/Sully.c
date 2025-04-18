051d7ed7c5245446d06efba691297a421ea1d907
Valentin HEMERET <vahemere@paul-f3Ar2s4.clusters.42paris.fr>
2025-04-18 11:19:20 +0200

    add | #define _GNU_SOURCE in Sully.c

--- C/Sully.c
+++ C/Sully.c
@@ -1,10 +1,11 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
 	int x = 5;
 	if (x <= 0) return (0);
-	char *str = "#include <stdio.h>%c#include <stdlib.h>%c%cint main() {%c%cint x = %d;%c%cif (x <= 0) return (0);%c%cchar *str = %c%s%c;%c%cchar *filename = NULL;%c%cchar *compile_cmd = NULL;%c%cchar *exec_cmd = NULL;%c%cFILE *fd;%c%c%cif (asprintf(&filename, %cSully_%%d.c%c, x - 1) == -1) return (1);%c%cfd = fopen(filename, %cw%c);%c%cif (!fd) return (1);%c%cfprintf(fd, str, 10, 10, 10, 10, 9, x - 1, 10, 9, 10, 9, 34, str, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10);%c%cfclose(fd);%c%c%cif (asprintf(&compile_cmd, %cgcc -Wall -Wextra -Werror %%s -o Sully_%%d%c, filename, x - 1) == -1) return (1);%c%csystem(compile_cmd);%c%c%cif (asprintf(&exec_cmd, %c./Sully_%%d%c, x - 1) == -1) return (1);%c%csystem(exec_cmd);%c%c%cfree(filename);%c%cfree(compile_cmd);%c%cfree(exec_cmd);%c%creturn (0);%c}";
+	char *str = "#define _GNU_SOURCE%c#include <stdio.h>%c#include <stdlib.h>%c%cint main() {%c%cint x = %d;%c%cif (x <= 0) return (0);%c%cchar *str = %c%s%c;%c%cchar *filename = NULL;%c%cchar *compile_cmd = NULL;%c%cchar *exec_cmd = NULL;%c%cFILE *fd;%c%c%cif (asprintf(&filename, %cSully_%%d.c%c, x - 1) == -1) return (1);%c%cfd = fopen(filename, %cw%c);%c%cif (!fd) return (1);%c%cfprintf(fd, str, 10, 10, 10, 10, 10, 9, x - 1, 10, 9, 10, 9, 34, str, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10);%c%cfclose(fd);%c%c%cif (asprintf(&compile_cmd, %cgcc -Wall -Wextra -Werror %%s -o Sully_%%d%c, filename, x - 1) == -1) return (1);%c%csystem(compile_cmd);%c%c%cif (asprintf(&exec_cmd, %c./Sully_%%d%c, x - 1) == -1) return (1);%c%csystem(exec_cmd);%c%c%cfree(filename);%c%cfree(compile_cmd);%c%cfree(exec_cmd);%c%creturn (0);%c}";
 	char *filename = NULL;
 	char *compile_cmd = NULL;
 	char *exec_cmd = NULL;
@@ -13,7 +14,7 @@ int main() {
 	if (asprintf(&filename, "Sully_%d.c", x - 1) == -1) return (1);
 	fd = fopen(filename, "w");
 	if (!fd) return (1);
-	fprintf(fd, str, 10, 10, 10, 10, 9, x - 1, 10, 9, 10, 9, 34, str, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10);
+	fprintf(fd, str, 10, 10, 10, 10, 10, 9, x - 1, 10, 9, 10, 9, 34, str, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10);
 	fclose(fd);
 
 	if (asprintf(&compile_cmd, "gcc -Wall -Wextra -Werror %s -o Sully_%d", filename, x - 1) == -1) return (1);
